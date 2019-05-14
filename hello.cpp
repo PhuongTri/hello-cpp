3eb8d1180eabb3b1c1b51c5b2d7bcc23f39b509a
PhuongTri <50441189+PhuongTri@users.noreply.github.com>
2019-05-14 14:57:00 +0700

    ahihi

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main () {
+  cout<<"hello world"<<endl;  
+  cout<<1+1<<endl;  
+  cout<<"NINH"<<endl;  
+  cout << 1-1 <<endl;
+  cout<<"phong"<<endl;
+  return 0;
+}
